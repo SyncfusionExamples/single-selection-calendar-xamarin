fc88d735f846a8b0dc13e00d0810baea151b3493
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-07-02 17:40:28 +0530

    XAMARIN-34939 - single selection sample added.

---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/ViewModel/CalendarViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalendarXamarin
+{
+    public class CalendarViewModel
+    {
+        public DateTime SelectedDates { get; set; } 
+        public CalendarViewModel()
+        {
+            
+            SelectedDates =  new DateTime(2019, 03, 13);
+        }
+    }
+}
+
